dd0804d4286379799f35693d19fd3b5a811fb6e0
Susanne <35730140+BettinaSDe@users.noreply.github.com>
2018-05-26 22:14:58 +0200

    added timer and moves to JS

--- js/app.js
+++ js/app.js
@@ -96,6 +96,24 @@
          };
 
 
+
+
+
+
+
+         
+         
+
+
+
+
+
+
+
+
+
+
+
          /*  adding reset game functionality
           Get the restart button */
 
@@ -136,6 +154,19 @@ let cardClicks = 0;
 // Since there is no any clicks at the beginning you have to set this
 // to true as initial value otherwise you won't even be able to click cards
 let actionCompleted = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
 function cardSelectHandler(event) {
 
     // If current cards are still open do not open cards until
@@ -218,12 +249,169 @@ function assignCardHandler() {
 }
 
 
-/*function start is true */
-startGame(true);
+/*function start is true 
+startGame(true);   */
 
-         
+   
+//first click to start timer
+let isFirstClick = true;
+
+function cardSelectHandler(event) {
+
+//Show the card's picture on click
+
+  if (isFirstClick) {
+    startTimer();
+    isFirstClick = false;
+  }
 
 
+ // Add New Move
+    addMove();
+}
+
+/*
+ * Check if the game is over!
+ */
+function isOver() {
+    if(permanentChecks.length === memory.length) {
+
+        // Stop our timer
+        stopTimer();
+
+        /*
+         * Display your popup here, the `alert` is for explanation only!
+         * 
+         * In your popup, you should create a button, 
+         * To let the user play a new game
+         * 
+         * After clicking on that button, you should:
+         *  - Call the `init` function to re-create the cards
+         *  - Call the `reset` function to reset all variables
+         */
+        alert("GAME OVER!");
+        
+    }
+}
+
+
+/*
+ * Add move
+ */
+const movesContainer = document.querySelector(".moves");
+let moves = 0;
+movesContainer.innerHTML = 0;
+function addMove() {
+    moves++;
+    movesContainer.innerHTML = moves;
+
+    // Set the rating
+    rating();
+}
+
+/*
+ * Rating
+ */
+const starsContainer = document.querySelector(".stars");
+const star = `<li><i class="fa fa-star"></i></li>`;
+starsContainer.innerHTML = star + star + star;
+function rating() {
+
+    if( moves < 10) {
+        starsContainer.innerHTML = star + star + star;
+    } else if( moves < 15) {
+        starsContainer.innerHTML = star + star;
+    } else {
+        starsContainer.innerHTML = star;
+    }
+}
+
+
+/*
+ * Timer
+ */
+const timerContainer = document.querySelector(".timer");
+let liveTimer,
+    totalSeconds = 0;
+
+// Set the default value to the timer's container
+timerContainer.innerHTML = totalSeconds + 's';
+
+/*
+ * We call this function to start our function, 
+ * the totalSeconds will be increased 
+ * by 1 after 1000ms (1 second!)
+ * 
+ * HINT: We need to call this function ONCE, and the best time to call it
+ * is when the user click on a card (The first card!)
+ * This means that our user is start playing now! ;)
+ */
+ function startTimer() {
+    liveTimer = setInterval(function() {
+        // Increase the totalSeconds by 1
+        totalSeconds++;
+        // Update the HTML Container with the new time
+        timerContainer.innerHTML = totalSeconds + 's';
+    }, 1000);
+}
+
+/*
+ * Our timer won't stop. To stop it, we should clearInterval!
+ * We will call it when the game is over.
+ * So, we will call it at the end of `isOver` function
+ * 
+ * HINT: That's why I created the `liveTimer` variable, 
+ * to store the setInterval's function, so that we can stop it by its name!
+ */
+function stopTimer() {
+    clearInterval(liveTimer);
+}
+
+/* *//*
+ * Restart Button
+ *//*
+const restartBtn = document.querySelector(".restart");
+restartBtn.addEventListener("click", function() {
+    // Delete ALL cards
+    cardsContainer.innerHTML = "";
+
+    // Call `init` to create new cards
+    init();
+
+    // Reset the game
+    reset();
+
+});
+
+*/
+
+/*
+ * Reset All Game Variables
+ */
+function reset() {
+    // Empty the `matchedCards` array
+    matchedCards = [];
+
+    // Reset `moves`
+    moves = 0;
+    movesContainer.innerHTML = moves;
+
+    // Reset `rating`
+    starsContainer.innerHTML = star + star + star;
+
+    /*
+     * Reset the `timer`
+     * 
+     * - Stop it first
+     * - Then, reset the `isFirstClick` to `true` to be able to start the timer again!
+     * - Don't forget about `totalSeconds`, it must be `0`
+     * - One more thing, is to update the HTML timer's container
+     */
+    stopTimer();
+    isFirstClick = true;
+    totalSeconds = 0;
+    timerContainer.innerHTML = totalSeconds + "s";
+}
 
 
 
@@ -253,3 +441,14 @@ startGame(true);
            modal.style.display = "none";
        }
    }
+
+
+
+
+
+   /*function start is true */
+startGame(true);
+
+
+
+
